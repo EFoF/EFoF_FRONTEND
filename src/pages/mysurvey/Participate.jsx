a3646b6a95ce94c9e663ccaf1f2957d752aa025e
jeongyeonpark <jeongyeon865@gmail.com>
2023-05-09 21:25:43 +0900

    ES-120 [ADD] 내가 참여한 설문 리스트

---
+++ src/pages/mysurvey/Participate.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import FooterOne from '../../common/Footer';
+import HeaderOne from '../../common/Header';
+import BcrumbBannerOne from '../../elements/breadcrumb/BcrumbBannerOne';
+import ColorSwitcher from '../../elements/switcher/ColorSwitcher';
+import SEO from '../../common/SEO';
+import CtaLayoutOne from '../../component/CtaLayout';
+import ParticipateSurvey from '../../component/survey/ParticipateSurvey';
+
+
+const Participate = () => {
+
+    return (
+        <>
+            <SEO title="Project Four Column" />
+            <ColorSwitcher />
+            <main className="main-wrapper">
+                <HeaderOne />
+                <BcrumbBannerOne
+                    title="내가 참여한 설문"
+                    paragraph =""
+                    styleClass=""
+                    mainThumb="/images/banner/banner-thumb-1.png"
+                />
+                <ParticipateSurvey colSize="col-xl-3 col-lg-4 col-md-6" itemShow="12" columnGap="row-15" parentClass="project-column-4"/>
+                <CtaLayoutOne />
+                <FooterOne parentClass="" />
+            </main>
+        </>
+    )
+}
+
+export default Participate;
