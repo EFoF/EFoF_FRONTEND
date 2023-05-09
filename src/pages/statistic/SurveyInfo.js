da87a76f4135cdcc62f6d933d2c7e0706b9166e9
minkyung <minkyung7365@gmail.com>
2023-05-10 04:17:45 +0900

    폰화면깨짐 수정

--- src/pages/statistic/SurveyInfo.js
+++ src/pages/statistic/SurveyInfo.js
@@ -32,14 +32,14 @@ const SurveyInfo = ({projectStyle, portfolio}) => {
 						<img src={process.env.PUBLIC_URL + portfolio.image} alt="icon" />
 					</div> */}
 
-					<div className="content">
+					{/* <div className="content"> */}
 						<span className="subtitle">
 						{portfolio.category.map((cat, i) => (
 							<span key={i}>{cat}</span>
 						))}
 						</span>
 						<h3 className="title"> {portfolio.title}</h3>
-					</div>
+					{/* </div> */}
 				</div>
 			</div>
 			<div className='col-md-6'>
