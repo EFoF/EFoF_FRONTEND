4b03fb4e2f76e6ce3dbabae78167ef3b15918e46
jeongyeonpark <jeongyeon865@gmail.com>
2023-05-11 14:13:09 +0900

    ES-120 [REFACTOR] 참여 뷰 수정

--- src/pages/mysurvey/Participate.jsx
+++ src/pages/mysurvey/Participate.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import FooterOne from '../../common/Footer';
-import HeaderOne from '../../common/Header';
+import Footer from '../../common/Footer';
+import Header from '../../common/Header';
 import BcrumbBannerOne from '../../elements/breadcrumb/BcrumbBannerOne';
 import ColorSwitcher from '../../elements/switcher/ColorSwitcher';
 import SEO from '../../common/SEO';
-import CtaLayoutOne from '../../component/CtaLayout';
 import ParticipateSurvey from '../../component/survey/ParticipateSurvey';
 
 
@@ -15,16 +14,16 @@ const Participate = () => {
             <SEO title="Project Four Column" />
             <ColorSwitcher />
             <main className="main-wrapper">
-                <HeaderOne />
+                <Header />
                 <BcrumbBannerOne
+                    subtitle="Participated Survey"
                     title="내가 참여한 설문"
                     paragraph =""
                     styleClass=""
                     mainThumb="/images/banner/banner-thumb-1.png"
                 />
                 <ParticipateSurvey colSize="col-xl-3 col-lg-4 col-md-6" itemShow="12" columnGap="row-15" parentClass="project-column-4"/>
-                <CtaLayoutOne />
-                <FooterOne parentClass="" />
+                <Footer parentClass="" />
             </main>
         </>
     )
