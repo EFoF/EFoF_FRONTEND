2cb4b583993fc1080aefd671d09ca4092be56ecd
OwenChoi <oldstyle482@gmail.com>
2023-05-29 03:44:48 +0900

    [ADD] 설문 배경색 css 수정 중

--- src/component/TitleBox/Result/index.jsx
+++ src/component/TitleBox/Result/index.jsx
@@ -1,15 +1,10 @@
-import { Wrapper, InputWrapper, TitleInput, DetailInput, SettingButton } from './style';
-import { OptionButton } from '../../Question/OptionalQuestion/style';
+import { Wrapper, InputWrapper, TitleInput, DetailInput } from './style';
 import styled from 'styled-components';
-import { OptionsWrapper } from '../../Question/OptionalQuestion/style';
 import { useDispatch, useSelector } from 'react-redux';
-import { MdAdd, MdClose, MdPhoto, MdSettings } from 'react-icons/md';
-import { FaCheck, FaPalette } from 'react-icons/fa';
+import { MdAdd } from 'react-icons/md';
+import { FaCheck } from 'react-icons/fa';
 import { formActions } from '../../../slices';
 import ReactDOM from "react-dom";
-import ConfirmModal from '../../../ui/ConfirmModal';
-import axios from 'axios';
-import toastMsg from '../../../ui/Toast/index';
 import PaleteModal from '../../../ui/PaleteModal';
 
 const ResultTitleBox = ({ info, handleDetail, handleTitle, readOnly }) => {
@@ -24,19 +19,6 @@ const ResultTitleBox = ({ info, handleDetail, handleTitle, readOnly }) => {
   font-size: 1.3rem;
   align-self: center;
 `;
-    const handleClose = () => {
-        const handleConfirm = () => {
-            alert(JSON.stringify(form));
-            // ReactDOM.unmountComponentAtNode(document.getElementById("modal-root"));
-        };
-
-        const handleCancel = () => {
-            // 취소 버튼 클릭 시 처리할 코드 작성
-            ReactDOM.unmountComponentAtNode(document.getElementById("modal-root"));
-        };
-        const confirmModal = <PaleteModal message={"이미지를 삭제하시겠습니까?"} onConfirm={handleConfirm} onCancel={handleCancel} form={form} dispatch={dispatch} formActions={formActions}/>;
-        ReactDOM.render(confirmModal, document.getElementById("modal-root"));
-    };
 
 
     return (
