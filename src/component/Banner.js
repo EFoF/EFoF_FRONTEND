3d322361683c08a5f26cac789ab5be16fd693089
OwenChoi <oldstyle482@gmail.com>
2023-05-30 02:12:03 +0900

    [ADD] 설문 버튼 비로그인 회원 defensive

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {useSelector} from "react-redux";
+import toastMsg from "../ui/Toast";
+
 
 
 const Banner = () => {
+    const navigate = useNavigate();
+    const { loginLastDTO } = useSelector((state) => state.authorization);
+    const handleClick = () => {
+        const expiresDate = typeof(loginLastDTO.expiresAt) === "undefined" ?
+            new Date : new Date(loginLastDTO.expiresAt);
+        const currentDate = new Date();
+        if(currentDate >= expiresDate) {
+            // 로그인이 안되었거나 만료된 상태. 버튼을 막아야 한다.
+            // ReIssue는 이 버튼이 존재하는 상위 컴포넌트에서 수행해주니 여기서는 처리해줄 필요 없다.
+            toastMsg("로그인이 필요합니다.", false);
+            return;
+        } else {
+            navigate("/form");
+        }
+    }
+
     return (
         <div className="banner banner-style-1">
             <div className="container">
@@ -15,7 +34,7 @@ const Banner = () => {
                                 <h1 className="title">독수리 설문입니다!</h1>
                                 <br/>
                                 <span className="subtitle">독수리 설문을 통해<br/>더욱 편리한 설문 플랫폼을 경험해보세요 :)</span>
-                                <Link to="/form" className="axil-btn btn-fill-primary btn-large">설문 생성하기</Link>
+                                <button onClick={handleClick} className="axil-btn btn-fill-primary btn-large">설문 생성하기</button>
                             </AnimationOnScroll>
                         </div>
                     </div>
