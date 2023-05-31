3689401f4f77b66e533618fb21b8a0748ac83484
jyeon <jeongyeon865@gmail.com>
2023-05-31 20:39:57 +0900

    ES120 [REFACTOR] 참여 리스트 수정

--- src/pages/mysurvey/ParticipateOver.js
+++ src/pages/mysurvey/ParticipateOver.js
@@ -34,7 +34,7 @@ const ParticipateOver = () => {
                 {
                     visibleItems.map((data) => (
                         <div className="col-xl-3 col-lg-4 col-md-6" key={data.id}>
-                            <SurveyProp projectStyle="" survey={data} />
+                            <SurveyProp projectStyle="" survey={data}/>
                         </div>
                     ))}
             </div>
