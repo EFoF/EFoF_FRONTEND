6f689436b3aa2218c309b600e445074e74b93d27
jay <wlguswhd0809@naver.com>
2023-06-01 10:15:23 +0900

    [REFACTOR] Header / 마이페이지 삭제 & 홈 버튼 글씨 수정

--- src/ui/common/Header.js
+++ src/ui/common/Header.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Logo from '../../elements/logo/Logo';
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {FaAngleDown, FaUser} from "react-icons/fa";
 import Cookies from "js-cookie";
 import useLogin from "../../hooks/useLogin";
@@ -8,20 +8,21 @@ import axios from "axios";
 import API from '../../api/config';
 import {useDispatch, useSelector} from "react-redux";
 import {authorizationActions} from "../../slices/authorization";
+
 const Header = () => {
     const confirmCookie = Cookies.get("tokenPublishConfirm");
     const [loginState, setLoginState] = useState(false);
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
-    const { loginLastDTO } = useSelector((state) => state.authorization);
+    const {loginLastDTO} = useSelector((state) => state.authorization);
 
     const callReissue = () => {
         console.dir(loginLastDTO);
-        const expiresDate = typeof(loginLastDTO.expiresAt) === "undefined" ?
+        const expiresDate = typeof (loginLastDTO.expiresAt) === "undefined" ?
             new Date : new Date(loginLastDTO.expiresAt);
         const currentDate = new Date();
-        if(currentDate >= expiresDate) {
+        if (currentDate >= expiresDate) {
             console.log("ReIssue 시도")
             axios.post(`${API.REISSUE}`)
                 .then(async response => {
@@ -90,7 +91,11 @@ const Header = () => {
                         <div className="header-navbar">
                             <div className="header-logo">
                                 <ul className="mainmenu">
-                                    <li><Link to={process.env.PUBLIC_URL + "/"}>독수리 설문</Link></li>
+                                    <li>
+                                        <Link to={process.env.PUBLIC_URL + "/"}
+                                              style={{fontWeight: 'bold', color:'blue'}}>DOKSEOL
+                                        </Link>
+                                    </li>
                                 </ul>
                             </div>
                             <div className="header-main-nav">
@@ -98,33 +103,34 @@ const Header = () => {
                                     {loginState ? (
                                         <li>{loginLastDTO.nickname}</li>
                                     ) : (
-                                      <></>
+                                        <></>
                                     )}
                                     <li className="menu-item-has-children">
                                         {
                                             !loginState ?
-                                            (
-                                                <>
-                                                    <li><Link to="/login">로그인/회원가입</Link></li>
-                                                </>
-                                            ) : (
-                                                <div style={{display:"flex", flexDirection: "row"}}>
-                                                    <a style={myPageStyle}>
-                                                        <FaUser className="icon" style={iconStyle} />
-                                                        <span style={textStyle}>
-                                                            <FaAngleDown className="arrow" style={arrowStyle} />
+                                                (
+                                                    <>
+                                                        <li><Link to="/login">로그인/회원가입</Link></li>
+                                                    </>
+                                                ) : (
+                                                    <div style={{display: "flex", flexDirection: "row"}}>
+                                                        <a style={myPageStyle}>
+                                                            <FaUser className="icon" style={iconStyle}/>
+                                                            <span style={textStyle}>
+                                                            <FaAngleDown className="arrow" style={arrowStyle}/>
                                                 </span>
-                                                    </a>
-                                                    <ul className="axil-submenu" style={submenuStyle}>
-                                                                <>
-                                                                    <li><Link to="/contact">마이페이지</Link></li>
-                                                                    <li><Link to="/form/generate">내가 생성한 설문</Link></li>
-                                                                    <li><Link to="/form/participate">내가 참여한 설문</Link></li>
-                                                                    <li><a onClick={useLogout} className="axil-btn btn-fill-white">로그아웃</a></li>
-                                                                </>
-                                                    </ul>
-                                                </div>
-                                            )}
+                                                        </a>
+                                                        <ul className="axil-submenu" style={submenuStyle}>
+                                                            <>
+                                                                {/*<li><Link to="/contact">마이페이지</Link></li>*/}
+                                                                <li><Link to="/form/generate">내가 생성한 설문</Link></li>
+                                                                <li><Link to="/form/participate">내가 참여한 설문</Link></li>
+                                                                <li><a onClick={useLogout}
+                                                                       className="axil-btn btn-fill-white">로그아웃</a></li>
+                                                            </>
+                                                        </ul>
+                                                    </div>
+                                                )}
                                     </li>
                                 </ul>
                             </div>
